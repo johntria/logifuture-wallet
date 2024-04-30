1ac92db9d8b0b54afbbd3ed9843585443d099186
John Triantafyllakis <john.3fyl@gmail.com>
2024-04-30 12:39:32 +0300

    Replaced annotation from DecimalMin which was configured with greater than zero with positive

--- src/main/java/com/logifuture/wallet/dto/wallet/request/RemoveFundsFromWalletRequest.java
+++ src/main/java/com/logifuture/wallet/dto/wallet/request/RemoveFundsFromWalletRequest.java
@@ -1,8 +1,8 @@
 package com.logifuture.wallet.dto.wallet.request;
 
 import com.logifuture.wallet.controller.rest.WalletRestController;
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
@@ -13,6 +13,6 @@ import java.math.BigDecimal;
  * @param amount Amount which will be removed from wallet
  */
 public record RemoveFundsFromWalletRequest(@NotNull(message = "User ID must not be null or empty") Long userId,
-                                           @DecimalMin(message = "Amount must be greater than 0",value = "0.0") BigDecimal amount
+                                           @Positive(message = "Amount must be greater than zero")  BigDecimal amount
 ) {
 }
